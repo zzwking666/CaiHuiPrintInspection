512197bd268c77eaff3da846773c2530be396470
zfkj4090 <zfkj4090@DESKTOP-GJ77VA6>
2026-05-05 13:22:35 +0800

    添加项目文件。

---
+++ MaiLiDingZi/include/Modules/UIModule.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <QCheckBox>
+#include <QObject>
+#include "IModule.hpp"
+
+class MaiLiDingZi;
+class DlgProductSet;
+
+class UIModule
+	: public QObject, public IModule<void>
+{
+	Q_OBJECT
+public:
+	void build() override;
+	void destroy() override;
+	void start() override;
+	void stop() override;
+public:
+
+public:
+	DlgProductSet* _dlgProductSet = nullptr;
+	MaiLiDingZi* _maiLiDingZi = nullptr;
+};
