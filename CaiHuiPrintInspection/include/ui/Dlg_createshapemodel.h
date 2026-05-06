29be611cb942c94dc8d4e6fbdccc0f9f85c8f507
zfkj4090 <zfkj4090@DESKTOP-GJ77VA6>
2026-05-06 15:41:08 +0800

    新增创建形状模板对话框及相关UI和类定义
    新增Dlg_createshapemodel.h/cpp，定义并实现Dlg_createshapemodel类及其槽函数。新增Dlg_createshapemodel.ui，设计包含图像显示、参数设置、区域绘制、模板创建等功能的复杂对话框界面，完善控件布局与样式。

---
+++ CaiHuiPrintInspection/include/ui/Dlg_createshapemodel.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "ui_Dlg_createshapemodel.h"
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Dlg_createshapemodelClass; };
+QT_END_NAMESPACE
+
+class Dlg_createshapemodel : public QDialog
+{
+	Q_OBJECT
+public:
+	Dlg_createshapemodel(QWidget* parent = nullptr);
+	~Dlg_createshapemodel();
+
+public:
+	void build_ui();
+	void build_connect();
+
+private slots:
+	void btn_exit_clicked();
+	void btn_readImage_clicked();
+	void btn_paintRegion_clicked();
+
+	void btn_createShapeModel_clicked();
+	void btn_shiledRegion_clicked();
+	void btn_clearRegion_clicked();
+	void btn_paintCenterPoint_clicked();
+
+	void rad_findshapemodel_toggled(bool checked);
+	void rad_findshapemodelXLD_toggled(bool checked);
+
+	void btn_MinLength_clicked();
+	void btn_min_clicked();
+	void btn_max_clicked();
+	void btn_createXLD_clicked();
+
+	void rbtn_auto_toggled(bool checked);
+	void rbtn_manual_toggled(bool checked);
+	void btn_contrast_clicked();
+	void btn_mincontrast_clicked();
+
+	void btn_clearRegion2_clicked();
+	void btn_zengyi_clicked();
+	void btn_baoguang_clicked();
+	void btn_angle_clicked();
+	void btn_opening_clicked();
+	void btn_mean_clicked();
+public:
+	Ui::Dlg_createshapemodelClass* ui;
+};
---
+++ CaiHuiPrintInspection/src/ui/Dlg_createshapemodel.cpp
@@ -0,0 +1,109 @@
+#include "Dlg_createshapemodel.h"
+
+Dlg_createshapemodel::Dlg_createshapemodel(QWidget* parent)
+{
+}
+
+Dlg_createshapemodel::~Dlg_createshapemodel()
+{
+}
+
+void Dlg_createshapemodel::build_ui()
+{
+}
+
+void Dlg_createshapemodel::build_connect()
+{
+}
+
+void Dlg_createshapemodel::btn_exit_clicked()
+{
+}
+
+void Dlg_createshapemodel::btn_readImage_clicked()
+{
+}
+
+void Dlg_createshapemodel::btn_paintRegion_clicked()
+{
+}
+
+void Dlg_createshapemodel::btn_createShapeModel_clicked()
+{
+}
+
+void Dlg_createshapemodel::btn_shiledRegion_clicked()
+{
+}
+
+void Dlg_createshapemodel::btn_clearRegion_clicked()
+{
+}
+
+void Dlg_createshapemodel::btn_paintCenterPoint_clicked()
+{
+}
+
+void Dlg_createshapemodel::rad_findshapemodel_toggled(bool checked)
+{
+}
+
+void Dlg_createshapemodel::rad_findshapemodelXLD_toggled(bool checked)
+{
+}
+
+void Dlg_createshapemodel::btn_MinLength_clicked()
+{
+}
+
+void Dlg_createshapemodel::btn_min_clicked()
+{
+}
+
+void Dlg_createshapemodel::btn_max_clicked()
+{
+}
+
+void Dlg_createshapemodel::btn_createXLD_clicked()
+{
+}
+
+void Dlg_createshapemodel::rbtn_auto_toggled(bool checked)
+{
+}
+
+void Dlg_createshapemodel::rbtn_manual_toggled(bool checked)
+{
+}
+
+void Dlg_createshapemodel::btn_contrast_clicked()
+{
+}
+
+void Dlg_createshapemodel::btn_mincontrast_clicked()
+{
+}
+
+void Dlg_createshapemodel::btn_clearRegion2_clicked()
+{
+}
+
+void Dlg_createshapemodel::btn_zengyi_clicked()
+{
+}
+
+void Dlg_createshapemodel::btn_baoguang_clicked()
+{
+}
+
+void Dlg_createshapemodel::btn_angle_clicked()
+{
+}
+
+void Dlg_createshapemodel::btn_opening_clicked()
+{
+}
+
+void Dlg_createshapemodel::btn_mean_clicked()
+{
+}
