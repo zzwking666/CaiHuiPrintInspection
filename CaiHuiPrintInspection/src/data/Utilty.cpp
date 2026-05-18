357aaf0d0b2d6dead3f351f6eae962eca33622a8
zfkj4090 <zfkj4090@DESKTOP-GJ77VA6>
2026-05-18 14:04:19 +0800

    更新 Utility 类中 cameraIp1 和 cameraIp2 的初始值
    将 cameraIp1 的初始值由 "1" 改为 "3"，cameraIp2 的初始值由 "2" 改为 "4"，以适应新的相机 IP 配置需求。

--- CaiHuiPrintInspection/src/data/Utilty.cpp
+++ CaiHuiPrintInspection/src/data/Utilty.cpp
@@ -1,7 +1,7 @@
 #include "Utilty.hpp"
 
-QString Utility::cameraIp1 = "1";
-QString Utility::cameraIp2 = "2";
+QString Utility::cameraIp1 = "3";
+QString Utility::cameraIp2 = "4";
 
 std::unordered_map<rw::imgPro::ClassId, rw::imgPro::ClassIdName> ClassId::classIdNameMap = {
 	{ClassId::dingZi,"钉子"}
